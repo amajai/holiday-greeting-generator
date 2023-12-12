5c6e57604a2007a9699c113c17f38efa0ee43b98
Abdul <aabdulmajeed.isa@gmail.com>
2023-12-12 01:30:09 +0100

    add loader and data display section

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+    $('#output').hide()
+    $('#loader').hide()
     $('#form').on('submit', function(e){
-        $('#output').text('loading...');
+        $('#output').hide()
+        $('#loader').show()
         $.ajax({
         data : {
             holiday : $('#holiday').val(),
@@ -16,9 +19,13 @@ $(document).ready(function() {
         })
         .done(async function(data){
             d = await data
+            $('#loader').hide()
+            $('#output').show()
             $('#output').html(d.replace(/\n/g, '<br>'));
         })
-        .fail(function(e){
+        .fail(function(e){ 
+            $('#loader').hide()
+            $('#output').show()
             $('#output').text('failed!');
         });
         e.preventDefault();
